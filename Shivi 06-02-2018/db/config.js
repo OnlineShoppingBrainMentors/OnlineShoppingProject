e8ab03ce1d911483b85dfdf0e7b6b7a58d697e2a
shivi123gangwar <32398611+shivi123gangwar@users.noreply.github.com>
2018-02-06 15:13:07 +0530

    Add files via upload

---
+++ Shivi 06-02-2018/db/config.js
@@ -0,0 +1,4 @@
+const URL={
+     url:"Documents/OnlineShopping/admin/public/views/admin"
+}
+module.exports=URL;
---
+++ Shivi 06-02-2018/public/js/admin/registercontroller.js
@@ -0,0 +1,31 @@
+app.controller("registercontroller",function($scope,adminFactory,REGISTERURL){
+    $scope.register=function(){
+        var name = $scope.name;
+        var emailid = $scope.emailid;
+        var password = $scope.password;
+        var phoneno = $scope.phoneno;
+        var dob=$scope.dob;
+        /*var photo=$scope.photo;
+        var signature=$scope.signature;*/
+        console.log(phoneno+""+dob+""+signature);
+        const adminObj = {"name":name,"phoneno":phoneno,"emailid":emailid,"password":password,"dob":dob};
+        console.log(adminObj);
+        var promise = adminFactory.doAjaxCall(REGISTERURL,adminObj);
+        promise.then(function(data){
+            $scope.data = data;
+        },function(error){
+            $scope.err = "some server problem"+error;
+        });
+    }
+
+    $scope.validate=(formName)=>{
+        if(formName.$valid){
+            $scope.result = "Form is Valid..."
+        } 
+        else{
+            $scope.result = "Invaid Form ";
+        }      
+    }
+
+
+});
