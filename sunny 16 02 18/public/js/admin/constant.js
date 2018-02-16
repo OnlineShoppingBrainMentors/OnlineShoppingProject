52bef45bcc65fc4b392650dd4357ef0c418761b4
OnlineShoppingBrainMentors <35493160+OnlineShoppingBrainMentors@users.noreply.github.com>
2018-02-16 12:53:37 +0530

    Add files via upload

---
+++ sunny 16 02 18/public/js/admin/constant.js
@@ -0,0 +1,4 @@
+//app.value("URL","http://localhost:3000/js/json/fake.json");
+//app.value("MENUURL","http://localhost:3000/js/json/menu.json");
+app.constant("DATAURL","http://localhost:3000/js/json/fake.json");
+app.constant("MENUURL","http://localhost:3000/js/json/menu.json");
