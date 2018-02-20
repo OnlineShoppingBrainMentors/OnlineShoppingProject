ec6c2829e2cffb927d3c4dfdb1aeb5c01c8810a7
nvjot94 <nvjot94@gmail.com>
2018-02-20 16:33:51 +0530

    Add files via upload

---
+++ navjot 20 08 18/sunny with spa/js/config/urlconfig.js
@@ -0,0 +1,5 @@
+app.constant("DATAURL","http://localhost:3000/json/fake.json");
+
+app.constant("MENUURL","http://localhost:3000/json/menu.json");
+
+app.constant("PRODUCTURL","http://localhost:3000/json/products.json");
