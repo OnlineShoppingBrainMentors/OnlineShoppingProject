3bbf22bedcec93da6dd7c15ea2aee32950fa14d5
shivi123gangwar <32398611+shivi123gangwar@users.noreply.github.com>
2018-03-18 16:11:13 +0530

    Add files via upload

---
+++ shivi 18-03-2018/js/item.js
@@ -0,0 +1,20 @@
+class item{
+    constructor(id,img,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14){
+        this.id = id;
+        this.img = img;
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f3 = f3;
+        this.f4 = f4;
+        this.f5 = f5;
+        this.f6 = f6;
+        this.f7 = f7;
+        this.f8 = f8;
+        this.f9 = f9;
+        this.f10 = f10;
+        this.f11 = f11;
+        this.f12 = f12;
+        this.f13 = f13;
+        this.f14 = f14;
+    }
+}
